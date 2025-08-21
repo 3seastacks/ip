a9c32c56828b51507ad8cf1e89f23dbfed2214aa
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-08-21 20:24:59 +0800

    Add Stella.java

---
+++ src/main/java/Stella.java
@@ -0,0 +1,10 @@
+public class Stella {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Stella");
+        System.out.println("What can I do for you?");
+
+        System.out.println("Goodbye!");
+
+
+    }
+}
