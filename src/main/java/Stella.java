60fdfd69a94c41b0af6a0c01196a564745c757fa
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-08-21 21:28:13 +0800

    Add storage and display feature

--- src/main/java/Stella.java
+++ src/main/java/Stella.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Stella {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        String[] lists = new String[100];
+        int counter = 0;
+
         System.out.println(" > Hello! I am Stella");
         System.out.println(" > What can I do for you?");
 
         String user_input = scan.nextLine();
 
         while (!user_input.equals("bye")) {
-            System.out.println(" > " + user_input);
+            if (user_input.equals(("list"))) {
+               for (int i = 1; i - 1 < counter; i = i + 1) {
+                   System.out.println(i + ". " + lists[i-1]);
+               }
+            }
+            else {
+                lists[counter] = user_input;
+                counter = counter + 1;
+                System.out.println(" > added: " + user_input);
+            }
+
             user_input = scan.nextLine();
 
         }
