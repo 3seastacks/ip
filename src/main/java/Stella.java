d1abd52f55acfc04f373c642097ddc573244cc7c
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-08-21 20:58:48 +0800

    Add echo feature

--- src/main/java/Stella.java
+++ src/main/java/Stella.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Stella {
     public static void main(String[] args) {
-        System.out.println("Hello! I am Stella");
-        System.out.println("What can I do for you?");
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" > Hello! I am Stella");
+        System.out.println(" > What can I do for you?");
 
-        System.out.println("Goodbye!");
+        String user_input = scan.nextLine();
+
+        while (!user_input.equals("bye")) {
+            System.out.println(" > " + user_input);
+            user_input = scan.nextLine();
+
+        }
+
+        System.out.println(" > Goodbye!");
 
 
     }
