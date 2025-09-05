21491f7c5dbd4fc9434cf86fa0919f2e658c4772
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-09-05 16:51:01 +0800

    Add GUI framework

---
+++ src/main/java/stella/Main.java
@@ -0,0 +1,31 @@
+package stella;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Stella using FXML.
+ */
+public class Main extends Application {
+
+    private Stella stella = new Stella();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setStella(stella);  // inject the Stella instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
